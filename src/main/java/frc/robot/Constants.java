714600c426f0124f14de89d9e8aaa364d63cdbd5
Baezor <64096464+Baezor1@users.noreply.github.com>
2023-03-23 15:10:55 -0500

    Arm constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,10 +1,25 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
 public class Constants {
 
   // Speed Multiplier Variables
   public final double minMultiplier = 0.55;
   public final double maxMultiplier = 1;//0.85;
 
+  // Arm/Claw Speed
+  public final double armUpSpeed = 0.3;
+  public final double armDownSpeed = -0.1;
+  
+  public final double clawOpenSpeed = 0.2;
+  public final double clawCloseSpeed = -0.2;
+
+  // Arm Motors
+  public final PWMSparkMax leftArmMotor = new PWMSparkMax(3);
+  public final PWMSparkMax rightArmMotor = new PWMSparkMax(4);
+
+  // Claw Motors
+  public final PWMSparkMax clawMotor = new PWMSparkMax(6);
     
 }
